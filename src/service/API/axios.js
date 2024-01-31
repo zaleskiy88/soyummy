d419caf177fa92cef54fa3ee621a08ca38b15bc3
zaleskiy88 <zaleskiy88@gmail.com>
2024-01-31 19:24:54 +0000

    updated baseURL in axios instance

--- src/service/API/axios.js
+++ src/service/API/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const { REACT_APP_BASE_BACKEND_URL } = process.env;
 
 export const axiosInstance = axios.create({
-  baseURL: REACT_APP_BASE_BACKEND_URL,
+  baseURL: "https://troubled-teal-fedora.cyclic.app",
 });
