536b5f619962a54019c72629e63492f8ddd12c13
zaleskiy88 <78390754+zaleskiy88@users.noreply.github.com>
2024-09-07 14:17:19 +0100

    Update baseURL in axiosInsatnce

--- src/service/API/axios.js
+++ src/service/API/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://troubled-teal-fedora.cyclic.app",
+  baseURL: "https://souyummy-backend-yk52.onrender.com",
 });
